90e42fa68c5087bae6faa8eccf5433104020075d
sunjh <1455364690@qq.com>
2020-03-15 16:12:37 +0800

    添加注释

--- src/string/替换空格.java
+++ src/string/替换空格.java
@@ -4,6 +4,11 @@ package string;
  * @author sunjh
  * @date 2020/3/15 16:08
  */
+/*
+请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+
+*/
 public class 替换空格 {
     public static void main(String[] args) {
 
